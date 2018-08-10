dd190d9088db3c7c46b01d2fc42d2be9fafbc56a
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-10 17:07:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function distanceFromHqInFeet(someValue) {
   return (distanceFromHqInBlocks(someValue) * 264)
 }
 
-function distanceTravelledInFeet(someValue, someValue) {
-  return (Math.abs(someValue-someValue) * 264);
+function distanceTravelledInFeet(someValue, anotherValue) {
+  return (Math.abs(someValue-anotherValue) * 264);
 }
