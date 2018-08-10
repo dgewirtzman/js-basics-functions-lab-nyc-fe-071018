050f185dbab6bc1cf79679cea13e748174b477b2
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-10 17:31:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,15 +11,23 @@ function distanceTravelledInFeet(someValue, anotherValue) {
   return (Math.abs(someValue-anotherValue) * 264);
 }
 
+// function calculatesFarePrice(someValue, anotherValue) {
+//     let distance = distanceTravelledInFeet(someValue, anotherValue);
+//     if (distance < 401){
+//       return 0;
+//     } else if (distance <= 2000){
+//       return ((distance - 400) * .02);
+//     } else if ((distance > 2001) && (distance <= 2500)){
+//       return 25;
+//     } else {
+//       return 'cannot travel that far';
+//   }
+// }
+
 function calculatesFarePrice(someValue, anotherValue) {
     let distance = distanceTravelledInFeet(someValue, anotherValue);
-    if (distance < 401){
+    switch (distance){
+      case (distance <=400):
       return 0;
-    } else if (distance <= 2000){
-      return ((distance - 400) * .02);
-    } else if ((distance > 2001) && (distance <= 2500)){
-      return 25;
-    } else {
-      return 'cannot travel that far';
-   }
+    }
 }
