321f1888477f17b392e40c61f6c8514b790fa6a6
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-10 17:34:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ function calculatesFarePrice(someValue, anotherValue) {
         return ((distance - 400) * .02);
       case ((distance > 2001) && (distance <= 2500)):
         return 25;
-      
+      default:
+        return ''
       }
     
 }
